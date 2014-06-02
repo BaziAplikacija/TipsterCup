5fd53418fc73d29549fbe0bc46f1a905360d4c09
PetrovskiIgor <igor.tetovo@gmail.com>
2014-06-02 19:40:18 +0200

    login forma pocetna verzija

--- TipsterCup/FormLogin.Designer.cs
+++ TipsterCup/FormLogin.Designer.cs
@@ -29,10 +29,159 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lblLanguage = new System.Windows.Forms.Label();
+            this.cbLanguage = new System.Windows.Forms.ComboBox();
+            this.lblLoginAs = new System.Windows.Forms.Label();
+            this.cbLoginAs = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(54, 56);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(54, 101);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(144, 98);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(132, 20);
+            this.tbPassword.TabIndex = 2;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(144, 49);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(132, 20);
+            this.tbUsername.TabIndex = 1;
+            // 
+            // lblLanguage
+            // 
+            this.lblLanguage.AutoSize = true;
+            this.lblLanguage.Location = new System.Drawing.Point(54, 189);
+            this.lblLanguage.Name = "lblLanguage";
+            this.lblLanguage.Size = new System.Drawing.Size(58, 13);
+            this.lblLanguage.TabIndex = 4;
+            this.lblLanguage.Text = "Language:";
+            // 
+            // cbLanguage
+            // 
+            this.cbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLanguage.FormattingEnabled = true;
+            this.cbLanguage.Items.AddRange(new object[] {
+            "ENG",
+            "МКД"});
+            this.cbLanguage.Location = new System.Drawing.Point(57, 226);
+            this.cbLanguage.Name = "cbLanguage";
+            this.cbLanguage.Size = new System.Drawing.Size(121, 21);
+            this.cbLanguage.TabIndex = 3;
+            this.cbLanguage.SelectedIndexChanged += new System.EventHandler(this.cbLanguage_SelectedIndexChanged);
+            // 
+            // lblLoginAs
+            // 
+            this.lblLoginAs.AutoSize = true;
+            this.lblLoginAs.Location = new System.Drawing.Point(253, 189);
+            this.lblLoginAs.Name = "lblLoginAs";
+            this.lblLoginAs.Size = new System.Drawing.Size(50, 13);
+            this.lblLoginAs.TabIndex = 6;
+            this.lblLoginAs.Text = "Login as:";
+            // 
+            // cbLoginAs
+            // 
+            this.cbLoginAs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLoginAs.FormattingEnabled = true;
+            this.cbLoginAs.Items.AddRange(new object[] {
+            "Tipster",
+            "Administrator"});
+            this.cbLoginAs.Location = new System.Drawing.Point(256, 226);
+            this.cbLoginAs.Name = "cbLoginAs";
+            this.cbLoginAs.Size = new System.Drawing.Size(121, 21);
+            this.cbLoginAs.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(57, 302);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(256, 302);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // FormLogin
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormLogin";
+            this.ClientSize = new System.Drawing.Size(408, 358);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.cbLoginAs);
+            this.Controls.Add(this.lblLoginAs);
+            this.Controls.Add(this.cbLanguage);
+            this.Controls.Add(this.lblLanguage);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lblLanguage;
+        private System.Windows.Forms.ComboBox cbLanguage;
+        private System.Windows.Forms.Label lblLoginAs;
+        private System.Windows.Forms.ComboBox cbLoginAs;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
--- TipsterCup/FormLogin.cs
+++ TipsterCup/FormLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,173 @@ using System.Windows.Forms;
 
 namespace TipsterCup
 {
+
+    public enum Language
+    {
+        ENGLISH,
+        MACEDONIAN
+    }
     public partial class FormLogin : Form
     {
+
+        // formata koja kje se pojavi otkako validno kje se popolni Login formata
+        FormMain frmMain;
+        FormAdmin frmAdmin;
+
+        public Language language = Language.ENGLISH;
+
+        // parametri potrebni za konekcija so bazata
+        String connString = "Data Source=(DESCRIPTION="
+             + "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1620))"
+             + "(CONNECT_DATA=(SERVICE_NAME=ORCL)));"
+             + "User Id=DBA_20132014L_GRP_022;Password=9427657;";
+        OracleConnection connection;
+        OracleCommand command; 
+
+
         public FormLogin()
         {
             InitializeComponent();
+
+            connection = new OracleConnection(connString);
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+            cbLanguage.SelectedIndex = 0;
+            cbLoginAs.SelectedIndex = 0;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            Application.Exit();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+
+            if (validInput()) { 
+                this.Hide();
+                if (cbLoginAs.SelectedIndex == 0)
+                {
+                    frmMain = new FormMain();
+
+                    frmMain.Show();
+                }
+                else
+                {
+                    frmAdmin = new FormAdmin();
+                    frmAdmin.Show();
+                }
+                
+                connection.Close();
+            }
+            else
+            {
+                String errorMessage = "";
+
+                if (language == Language.ENGLISH)
+                {
+                    errorMessage = "The input parameters are invalid.";
+                }
+                else
+                {
+                    errorMessage = "Внесените параметри не се валидни.";
+                }
+                MessageBox.Show(errorMessage);
+            }
+        }
+        // proveruva dali vlezot za username i password vsusnost postoi vo bazata
+        private Boolean validInput()
+        {
+            String username = tbUsername.Text;
+            String password = tbPassword.Text;
+
+            username = username.Trim();
+
+            if (username.Equals("") || password.Equals(""))
+            {
+                return false;
+            }
+
+            String fromTable = "";
+
+            if (cbLoginAs.SelectedIndex == 0)
+            {
+                fromTable = "Tipster";
+            }
+            else
+            {
+                fromTable = "Administrator";
+            }
+            String cmdText = "SELECT * FROM " + fromTable + " WHERE username = '" + username + "' and password = '" + password + "'";
+                            
+            command = new OracleCommand(cmdText, connection);
+            command.CommandType = CommandType.Text;
+            OracleDataReader dataReader = command.ExecuteReader();
+            dataReader = command.ExecuteReader();
+
+            if (!dataReader.Read())
+            {
+                
+                return false;
+            }
+            
+            return true;
+        }
+
+        private void cbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbLanguage.SelectedIndex == 1)
+            {
+                language = Language.MACEDONIAN;
+                changeLanguageInItems(true);
+            }
+            else
+            {
+                language = Language.ENGLISH;
+                changeLanguageInItems(false);
+            }
+        }
+
+        private void changeLanguageInItems(Boolean macedonian)
+        {
+            int selectedIndex = cbLoginAs.SelectedIndex;
+            cbLoginAs.Items.Clear();
+            if (macedonian)
+            {
+                lblLanguage.Text = "Јазик:";
+                lblLoginAs.Text = "Најави се како:";
+                lblUsername.Text = "Корисничко име:";
+                lblPassword.Text = "Лозинка:";
+                btnExit.Text = "Излез";
+                btnGo.Text = "Почни!";
+                this.Text = "Најава";
+
+                cbLoginAs.Items.Add("Типстер");
+                cbLoginAs.Items.Add("Администратор");
+                
+
+                
+            }
+            else
+            {
+                lblLanguage.Text = "Language:";
+                lblLoginAs.Text = "Login as:";
+                lblUsername.Text = "Username:";
+                lblPassword.Text = "Password:";
+                btnExit.Text = "Exit";
+                btnGo.Text = "Go!";
+                this.Text = "Login";
+
+                cbLoginAs.Items.Add("Tipster");
+                cbLoginAs.Items.Add("Administrator");
+            }
+
+            cbLoginAs.SelectedIndex = selectedIndex;
+
         }
     }
 }
