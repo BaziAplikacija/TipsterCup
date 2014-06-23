9a9c4e4c596ae0cc7a94aae53fdc43f96a6aa8e5
PetrovskiIgor <igor.tetovo@gmail.com>
2014-06-23 22:58:49 +0200

    FormAllTipsters gi prikazuva podredeni (spored rating) site tipsteri

---
+++ TipsterCup/FormAllTipsters.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TipsterCup
+{
+    partial class FormAllTipsters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridTipsters = new System.Windows.Forms.DataGridView();
+            this.colidTipster = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRank = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipster = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMoney = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTipsters)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridTipsters
+            // 
+            this.gridTipsters.AllowUserToAddRows = false;
+            this.gridTipsters.AllowUserToDeleteRows = false;
+            this.gridTipsters.BackgroundColor = System.Drawing.Color.White;
+            this.gridTipsters.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gridTipsters.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTipsters.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colidTipster,
+            this.colRank,
+            this.colTipster,
+            this.colMoney});
+            this.gridTipsters.Location = new System.Drawing.Point(91, 85);
+            this.gridTipsters.Name = "gridTipsters";
+            this.gridTipsters.ReadOnly = true;
+            this.gridTipsters.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridTipsters.Size = new System.Drawing.Size(384, 225);
+            this.gridTipsters.TabIndex = 0;
+            this.gridTipsters.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridTipsters_CellDoubleClick);
+            // 
+            // colidTipster
+            // 
+            this.colidTipster.HeaderText = "Id";
+            this.colidTipster.Name = "colidTipster";
+            this.colidTipster.ReadOnly = true;
+            this.colidTipster.Visible = false;
+            // 
+            // colRank
+            // 
+            this.colRank.HeaderText = "Rank";
+            this.colRank.Name = "colRank";
+            this.colRank.ReadOnly = true;
+            this.colRank.Width = 50;
+            // 
+            // colTipster
+            // 
+            this.colTipster.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colTipster.HeaderText = "Tipster";
+            this.colTipster.Name = "colTipster";
+            this.colTipster.ReadOnly = true;
+            // 
+            // colMoney
+            // 
+            this.colMoney.HeaderText = "Money/Rating";
+            this.colMoney.Name = "colMoney";
+            this.colMoney.ReadOnly = true;
+            // 
+            // FormAllTipsters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 356);
+            this.Controls.Add(this.gridTipsters);
+            this.Name = "FormAllTipsters";
+            this.Text = "FormAllTipsters";
+            this.Load += new System.EventHandler(this.FormAllTipsters_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridTipsters)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridTipsters;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colidTipster;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRank;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipster;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMoney;
+
+    }
+}
---
+++ TipsterCup/FormAllTipsters.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace TipsterCup
+{
+    public partial class FormAllTipsters : Form
+    {
+        public FormAllTipsters()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAllTipsters_Load(object sender, EventArgs e)
+        {
+            using (OracleConnection conn = new OracleConnection(FormLogin.connString))
+            {
+                conn.Open();
+
+                string query = "SELECT T.idTipster,(SELECT COUNT(*) FROM TIPSTER WHERE Rating > T.RATING) + 1 as rank, T.username, T.rating  FROM TIPSTER T  ORDER BY rank";
+
+                OracleCommand command = new OracleCommand(query, conn);
+                command.CommandType = CommandType.Text;
+
+
+                OracleDataReader reader = command.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    gridTipsters.Rows.Add(reader[0], reader[1], reader[2], reader[3]);
+                }
+
+                reader.Close();
+
+
+
+            }
+        }
+
+        private void gridTipsters_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int idTipster = Int32.Parse(gridTipsters[0, e.RowIndex].Value.ToString());
+            FormTipster frmTipster = new FormTipster(idTipster);
+            frmTipster.Show();
+        }
+    }
+}
