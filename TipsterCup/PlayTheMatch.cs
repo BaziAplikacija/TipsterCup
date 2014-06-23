bb401997be58adce7298dda892a15dcce307d139
goraniliev <goraniliev93@yahoo.com>
2014-06-24 01:18:49 +0200

    Ovozmozeno dodavanje na golovi i Participates vo bazata preku PlayTheMatch

--- TipsterCup/PlayTheMatch.cs
+++ TipsterCup/PlayTheMatch.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using System.Data;
+
 
 namespace TipsterCup
 {
@@ -33,20 +36,23 @@ namespace TipsterCup
             List<Player> guestTeam = mainDoc.getFirstEleven(match.HomeTeam.Name);
 
             int id = 0;//OVA TREBA DA GO DOBIJAM OD BAZATA
+
+
+
             totalAssistTokens = totalGoalTokens = totalInterruptTokens = totalSaveTokens = 0;
             foreach (Player p in homeTeam)
             {
                 AllPlayers.Add(p);
-                Participations.Add(new Participates(id, p, match, 0, 0, 0, 0));
-                totalAssistTokens += p.TokensAssists;
-                totalGoalTokens += p.TokensGoals;
-                totalInterruptTokens += p.TokensInterrupts;
-                totalSaveTokens += p.TokensSaves;
+                Participations.Add(new Participates(id, p, match, 0, 0, 0, 0));//vaka za site ucesnici se stava isto id
+                totalAssistTokens += p.TokensAssists;                     //no ovaa lista e samo lokalna za ovaa klasa
+                totalGoalTokens += p.TokensGoals;               //i toj index ne se koristi na drugi mesta, pa zatoa ne smeta
+                totalInterruptTokens += p.TokensInterrupts;//ne go izbrisav polet bidejki koga ke se zemaat site ucestva od glavnata forma
+                totalSaveTokens += p.TokensSaves;           //polet bidejki koga ke se zemaat site ucestva od glavnata forma ke bide potrebno i toa pole
             }
             foreach (Player p in guestTeam)
             {
                 AllPlayers.Add(p);
-                Participations.Add(new Participates(id, p, match, 0, 0, 0, 0));
+                Participations.Add(new Participates(id, p, match, 0, 0, 0, 0));//slicna diskusija kako pogore
                 totalAssistTokens += p.TokensAssists;
                 totalGoalTokens += p.TokensGoals;
                 totalInterruptTokens += p.TokensInterrupts;
@@ -56,11 +62,33 @@ namespace TipsterCup
             for (int minute = 1; minute <= 90; minute++)//0 - goal and eventually assist, 1 - interrupt, 2 save, nothing
             {
                 int happens = whatHappens();
-                int nextGoal = 0;//Ova go dobij od baza
+                int nextGoal = 0;//Slicno kako kaj Participates, sekade ke se stava nextGoal = 0, no toa ne pravi problem
                 if (happens == 0)//goal
                 {
                     int scorerId = goalScorer();
                     Goal goal = new Goal(nextGoal, minute, match, AllPlayers[scorerId]);
+                    //dodavanje na gol vo bazata
+
+                    using (OracleConnection conn = new OracleConnection(FormLogin.connString))
+                    {
+                        conn.Open();
+
+                        OracleCommand command = new OracleCommand("procInsertGoal", conn);
+                        command.CommandType = CommandType.StoredProcedure;
+                        /*
+                         *  p_idPlayer in Goal.idPlayer%TYPE,
+                         *  p_idMatch in Goal.idMatch%TYPE, 
+                         *  p_minutes in Goal.minutes%TYPE) 
+                         */
+                        command.Parameters.Add("p_idPlayer", OracleDbType.Int32).Value = goal.Player.Id;
+                        command.Parameters.Add("p_idMatch", OracleDbType.Int32).Value = goal.Match.Id;
+                        command.Parameters.Add("p_minutes", OracleDbType.Int32).Value = goal.Minutes;
+
+
+                        command.ExecuteNonQuery();
+
+                    }
+
                     Participations[scorerId].NumGoals++;
                     int assId = assistent();
                     if (assId != scorerId)
@@ -83,6 +111,33 @@ namespace TipsterCup
             foreach (Participates p in Participations)
             {
                 p.calculateMatchRating();
+                //dodavanje na ucestvata vo bazata
+                using (OracleConnection conn = new OracleConnection(FormLogin.connString))
+                {
+                    conn.Open();
+
+                    OracleCommand command = new OracleCommand("procInsertParticipates", conn);
+                    command.CommandType = CommandType.StoredProcedure;
+                    /*
+                     *  p_idPlayer in Participates.idPlayer%TYPE,
+                        p_idMatch in Participates.idMatch%TYPE, 
+                        p_numgoals in Participates.numgoals%TYPE, 
+                        p_numassists in Participates.numassists%TYPE, 
+                        p_numinterrupts in Participates.NUMINTERRUPTS%TYPE,  
+                        p_numsaves in Participates.numsaves%TYPE,
+                        p_matchrating in Participates.match_rating%TYPE) 
+                     */
+                    command.Parameters.Add("p_idPlayer", OracleDbType.Int32).Value = p.Player.Id;
+                    command.Parameters.Add("p_idMatch", OracleDbType.Int32).Value = p.Match.Id;
+                    command.Parameters.Add("p_numgoals", OracleDbType.Int32).Value = p.NumGoals;
+                    command.Parameters.Add("p_numassists", OracleDbType.Int32).Value = p.NumAssists;
+                    command.Parameters.Add("p_numinterrupts", OracleDbType.Int32).Value = p.NumInterrupts;
+                    command.Parameters.Add("p_numsaves", OracleDbType.Int32).Value = p.NumSaves;
+                    command.Parameters.Add("p_matchrating", OracleDbType.Int32).Value = p.MatchRating;
+
+                    command.ExecuteNonQuery();
+
+                }
             }
         }
 
