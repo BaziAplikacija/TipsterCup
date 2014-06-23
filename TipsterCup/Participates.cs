dfd4485d462a15fc2e6d2a2541067614d56303f4
goraniliev <goraniliev93@yahoo.com>
2014-06-23 13:19:49 +0200

    Dodadena klasa Participates

---
+++ TipsterCup/Participates.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipsterCup
+{
+    class Participates
+    {
+        public int IdParticipate { get; set; }
+        public Player Player { get; set; }
+        public Match Match { get; set; }
+        public int NumGoals { get; set; }
+        public int NumAssists { get; set; }
+        public int NumInterrupts { get; set; }
+        public int NumSaves { get; set; }
+
+        public int TmpRating { get; set; }//ke se koristi pri obnovuvanje na rejtingot na igracot
+
+        //verojatnosno-empirisko odreduvanje na najpogodni tezini, mozno e da ima i promeni
+        private const int WEIGHT_GOAL = 3000;//na site igraci ke bidat ednakvo vrednuvani site dostignuvanja
+        private const int WEIGHT_ASSIST = 2500;//nezavisno od pozicijata
+        private const int WEIGHT_INTERRUPT = 1000;//Tokens ke vlijaat koj kolku poeni ke dobiva od sto
+        private const int WEIGHT_SAVE = 800;//bidejki toa odreduva koj igrac kolkavi sansi ima sto da postigne
+                                            //vaka e po fer, bidejki na pr. gol e gol nezavisno dali igracot na
+                                            //koja pozicija igra
+
+        public Participates(int id, Player player, Match match, int ng, int na, int ni, int ns)
+        {
+            IdParticipate = id;
+            Player = player;
+            Match = match;
+            NumGoals = ng;
+            NumAssists = na;
+            NumInterrupts = ni;
+            NumSaves = ns;
+
+            TmpRating = NumGoals * WEIGHT_GOAL + NumAssists * WEIGHT_ASSIST + NumInterrupts * WEIGHT_INTERRUPT
+                        + NumSaves * WEIGHT_SAVE;
+        }
+
+
+    }
+}
