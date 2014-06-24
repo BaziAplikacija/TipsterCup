4e6402ca35445426daed7ae98cb0506014a1e873
VikiPeeva <peeva_viki@yahoo.com>
2014-06-24 14:45:43 +0200

    pak

---
+++ TipsterCup/FormMatchDetails.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace TipsterCup
+{
+    partial class FormMatchDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblRound = new System.Windows.Forms.Label();
+            this.lblVenue = new System.Windows.Forms.Label();
+            this.lblVenueInit = new System.Windows.Forms.Label();
+            this.lblRoundInit = new System.Windows.Forms.Label();
+            this.lblDateInit = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblCityInit = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.pbHomeTeam = new System.Windows.Forms.PictureBox();
+            this.lblHomeTeam = new System.Windows.Forms.Label();
+            this.pbGuestTeam = new System.Windows.Forms.PictureBox();
+            this.lblGuestTeam = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHomeTeam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGuestTeam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(221, 89);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date";
+            // 
+            // lblRound
+            // 
+            this.lblRound.AutoSize = true;
+            this.lblRound.Location = new System.Drawing.Point(221, 126);
+            this.lblRound.Name = "lblRound";
+            this.lblRound.Size = new System.Drawing.Size(39, 13);
+            this.lblRound.TabIndex = 3;
+            this.lblRound.Text = "Round";
+            // 
+            // lblVenue
+            // 
+            this.lblVenue.AutoSize = true;
+            this.lblVenue.Location = new System.Drawing.Point(221, 163);
+            this.lblVenue.Name = "lblVenue";
+            this.lblVenue.Size = new System.Drawing.Size(45, 13);
+            this.lblVenue.TabIndex = 4;
+            this.lblVenue.Text = "Stadium";
+            // 
+            // lblVenueInit
+            // 
+            this.lblVenueInit.AutoSize = true;
+            this.lblVenueInit.Location = new System.Drawing.Point(282, 163);
+            this.lblVenueInit.Name = "lblVenueInit";
+            this.lblVenueInit.Size = new System.Drawing.Size(35, 13);
+            this.lblVenueInit.TabIndex = 7;
+            this.lblVenueInit.Text = "label4";
+            // 
+            // lblRoundInit
+            // 
+            this.lblRoundInit.AutoSize = true;
+            this.lblRoundInit.Location = new System.Drawing.Point(282, 126);
+            this.lblRoundInit.Name = "lblRoundInit";
+            this.lblRoundInit.Size = new System.Drawing.Size(35, 13);
+            this.lblRoundInit.TabIndex = 6;
+            this.lblRoundInit.Text = "label5";
+            // 
+            // lblDateInit
+            // 
+            this.lblDateInit.AutoSize = true;
+            this.lblDateInit.Location = new System.Drawing.Point(282, 89);
+            this.lblDateInit.Name = "lblDateInit";
+            this.lblDateInit.Size = new System.Drawing.Size(35, 13);
+            this.lblDateInit.TabIndex = 5;
+            this.lblDateInit.Text = "label6";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(281, 26);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(66, 24);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "label7";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCityInit
+            // 
+            this.lblCityInit.AutoSize = true;
+            this.lblCityInit.Location = new System.Drawing.Point(282, 199);
+            this.lblCityInit.Name = "lblCityInit";
+            this.lblCityInit.Size = new System.Drawing.Size(35, 13);
+            this.lblCityInit.TabIndex = 12;
+            this.lblCityInit.Text = "label4";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(221, 199);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 11;
+            this.lblCity.Text = "City";
+            // 
+            // pbHomeTeam
+            // 
+            this.pbHomeTeam.Location = new System.Drawing.Point(22, 66);
+            this.pbHomeTeam.Name = "pbHomeTeam";
+            this.pbHomeTeam.Size = new System.Drawing.Size(145, 157);
+            this.pbHomeTeam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbHomeTeam.TabIndex = 14;
+            this.pbHomeTeam.TabStop = false;
+            // 
+            // lblHomeTeam
+            // 
+            this.lblHomeTeam.AutoSize = true;
+            this.lblHomeTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblHomeTeam.Location = new System.Drawing.Point(101, 26);
+            this.lblHomeTeam.Name = "lblHomeTeam";
+            this.lblHomeTeam.Size = new System.Drawing.Size(66, 24);
+            this.lblHomeTeam.TabIndex = 15;
+            this.lblHomeTeam.Text = "label8";
+            this.lblHomeTeam.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // pbGuestTeam
+            // 
+            this.pbGuestTeam.Location = new System.Drawing.Point(440, 66);
+            this.pbGuestTeam.Name = "pbGuestTeam";
+            this.pbGuestTeam.Size = new System.Drawing.Size(145, 157);
+            this.pbGuestTeam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGuestTeam.TabIndex = 16;
+            this.pbGuestTeam.TabStop = false;
+            // 
+            // lblGuestTeam
+            // 
+            this.lblGuestTeam.AutoSize = true;
+            this.lblGuestTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblGuestTeam.Location = new System.Drawing.Point(436, 26);
+            this.lblGuestTeam.Name = "lblGuestTeam";
+            this.lblGuestTeam.Size = new System.Drawing.Size(66, 24);
+            this.lblGuestTeam.TabIndex = 17;
+            this.lblGuestTeam.Text = "label9";
+            this.lblGuestTeam.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // FormMatchDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 331);
+            this.Controls.Add(this.pbGuestTeam);
+            this.Controls.Add(this.lblGuestTeam);
+            this.Controls.Add(this.pbHomeTeam);
+            this.Controls.Add(this.lblHomeTeam);
+            this.Controls.Add(this.lblCityInit);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblVenueInit);
+            this.Controls.Add(this.lblRoundInit);
+            this.Controls.Add(this.lblDateInit);
+            this.Controls.Add(this.lblVenue);
+            this.Controls.Add(this.lblRound);
+            this.Controls.Add(this.lblDate);
+            this.Name = "FormMatchDetails";
+            this.Text = "MatchDetails";
+            ((System.ComponentModel.ISupportInitialize)(this.pbHomeTeam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGuestTeam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblRound;
+        private System.Windows.Forms.Label lblVenue;
+        private System.Windows.Forms.Label lblVenueInit;
+        private System.Windows.Forms.Label lblRoundInit;
+        private System.Windows.Forms.Label lblDateInit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblCityInit;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.PictureBox pbHomeTeam;
+        private System.Windows.Forms.Label lblHomeTeam;
+        private System.Windows.Forms.PictureBox pbGuestTeam;
+        private System.Windows.Forms.Label lblGuestTeam;
+    }
+}
---
+++ TipsterCup/FormMatchDetails.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipsterCup
+{
+    public partial class FormMatchDetails : Form
+    {
+        Match match;
+
+        public FormMatchDetails()
+        {
+            InitializeComponent();
+        }
+
+        public FormMatchDetails(Match m)
+        {
+            InitializeComponent();
+            match = m;
+            init();
+        }
+
+        private void init()
+        {
+            lblHomeTeam.Text = match.HomeTeam.Name;
+            lblGuestTeam.Text = match.GuestTeam.Name;
+            String result = "-";
+            if (match.DateMatch.CompareTo(FormLogin.virtualDate) < 0)
+            {
+                result = match.GoalsHome.Count + " - " + match.GoalsGuest.Count;
+            }
+            lblResult.Text = result;
+            
+            pbHomeTeam.Image = match.HomeTeam.Picture;
+            pbGuestTeam.Image = match.GuestTeam.Picture;
+            
+            lblDateInit.Text = match.DateMatch.ToShortDateString();
+            lblRoundInit.Text = String.Format("{0}", match.Round.Id);
+            lblVenueInit.Text = match.HomeTeam.Stadium;
+            lblCityInit.Text = match.HomeTeam.City;
+
+            lblHomeTeam.Location = new Point(pbHomeTeam.Location.X + (pbHomeTeam.Width - lblHomeTeam.Width) / 2, lblHomeTeam.Location.Y);
+            lblGuestTeam.Location = new Point(pbGuestTeam.Location.X + (pbGuestTeam.Width - lblGuestTeam.Width) / 2, lblGuestTeam.Location.Y);
+        }
+    }
+}
