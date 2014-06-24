533984c6b29bbce868a89a0b244612d50e12b766
PetrovskiIgor <igor.tetovo@gmail.com>
2014-06-24 23:42:07 +0200

    sredeni transakcii 2

---
+++ TipsterCup/FormTransaction.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TipsterCup
+{
+    partial class FormTransaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTransaction));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message:";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(130, 106);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(145, 84);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // btnNo
+            // 
+            this.btnNo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNo.BackgroundImage")));
+            this.btnNo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNo.Location = new System.Drawing.Point(28, 237);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(50, 50);
+            this.btnNo.TabIndex = 4;
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGo.BackgroundImage")));
+            this.btnGo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGo.Location = new System.Drawing.Point(277, 237);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(50, 50);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(127, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(84, 13);
+            this.lblHeader.TabIndex = 6;
+            this.lblHeader.Text = "Send money to: ";
+            // 
+            // nudAmount
+            // 
+            this.nudAmount.Location = new System.Drawing.Point(130, 58);
+            this.nudAmount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(103, 20);
+            this.nudAmount.TabIndex = 7;
+            this.nudAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // FormTransaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 324);
+            this.Controls.Add(this.nudAmount);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTransaction";
+            this.Text = "FormTransaction";
+            this.Load += new System.EventHandler(this.FormTransaction_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.NumericUpDown nudAmount;
+    }
+}
---
+++ TipsterCup/FormTransaction.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+namespace TipsterCup
+{
+    public partial class FormTransaction : Form
+    {
+        private int receiverId;
+        public FormTipster frmParent;
+        public FormTransaction(int receiverId, String tipsterUsername)
+        {
+            InitializeComponent();
+            this.receiverId = receiverId;
+            this.lblHeader.Text = "Send money to tipster " + tipsterUsername;
+            
+        }
+
+        private void FormTransaction_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            using (OracleConnection conn = new OracleConnection(FormLogin.connString))
+            {
+                conn.Open();
+                int p_idTipsterFrom = FormLogin.IdLoggedTipster;
+                int p_idTipsterTo = receiverId;
+                int p_amount = (int)nudAmount.Value;
+                DateTime p_datetrans = DateTime.Now;
+                String p_message = tbMessage.Text;
+
+                //MessageBox.Show(p_datetrans);                                                                    //TO_DATE('" + to.Month + "/" + to.Day + "/" + to.Year + "', 'mm/dd/yyyy')
+                string query = "INSERT INTO Transaction(idTipsterFrom,idTipsterTo,datetrans,amount,message) VALUES(" + p_idTipsterFrom + ", " + p_idTipsterTo + ", TO_DATE('" + p_datetrans.Month + "/" + p_datetrans.Day + "/" + p_datetrans.Year + "', 'mm/dd/yyyy'), " +  + p_amount + " , '" + p_message + "')";
+                OracleCommand command = new OracleCommand(query, conn);
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+
+                // dodavanje na pari receiver tipster
+
+               
+                command = new OracleCommand("procIncMoney", conn);
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                
+                command.Parameters.Add("p_idTipster", OracleDbType.Int32).Value = p_idTipsterTo;
+                command.Parameters.Add("p_amount", OracleDbType.Int32).Value = p_amount;
+                command.ExecuteNonQuery();
+                // odzemanje na pari na receiver tipster
+
+                command = new OracleCommand("procIncMoney", conn);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add("p_idTipster", OracleDbType.Int32).Value = p_idTipsterFrom;
+                command.Parameters.Add("p_amount", OracleDbType.Int32).Value = -p_amount;
+                command.ExecuteNonQuery();
+
+
+
+            }
+
+            MessageBox.Show("The  transaction was successful.");
+            frmParent.Invalidate();
+            this.Close();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
