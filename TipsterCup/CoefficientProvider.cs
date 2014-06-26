ba6f4a49dab0caefa9a935022098d18a727b42eb
goraniliev <goraniliev93@yahoo.com>
2014-06-26 06:43:33 +0200

    Dodadena klasa CoefficientProvider

---
+++ TipsterCup/CoefficientProvider.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipsterCup
+{
+    class CoefficientProvider
+    {
+        private static double[] diff = {-0.5, 0, 0.5};
+        private static double[,] coeff = new double[16, 7];
+        private static int[] lowerBound = {500, 450, 400, 350, 300, 250, 200, 150, 100, 50, 0, 
+                        -50, -100, -150, -200, -250, -300, -350, -400, -450, -2000};
+        static Random random;
+
+        public CoefficientProvider()
+        {
+            coeff[0, 0] = 1.25;
+            coeff[0, 1] = 5.75;
+            coeff[0, 2] = 10.5;
+            coeff[0, 3] = 2.2;
+            coeff[0, 4] = 1.6;
+            coeff[0, 5] = 2.6;
+            coeff[0, 6] = 8.5;
+
+            coeff[1, 0] = 1.4;
+            coeff[1, 1] = 4.4;
+            coeff[1, 2] = 6.2;
+            coeff[1, 3] = 2.5;
+            coeff[1, 4] = 1.4;
+            coeff[1, 5] = 2;
+            coeff[1, 6] = 8.25;
+
+            coeff[2, 0] = 1.55;
+            coeff[2, 1] = 3.95;
+            coeff[2, 2] = 6.35;
+            coeff[2, 3] = 1.8;
+            coeff[2, 4] = 2.0;
+            coeff[2, 5] = 3.5;
+            coeff[2, 6] = 12.5;
+
+            coeff[3, 0] = 1.7;
+            coeff[3, 1] = 3.7;
+            coeff[3, 2] = 5.1;
+            coeff[3, 3] = 1.7;
+            coeff[3, 4] = 2.1;
+            coeff[3, 5] = 3.8;
+            coeff[3, 6] = 13.5;
+
+            coeff[4, 0] = 1.85;
+            coeff[4, 1] = 3.3;
+            coeff[4, 2] = 3.75;
+            coeff[4, 3] = 2.0;
+            coeff[4, 4] = 1.7;
+            coeff[4, 5] = 2.45;
+            coeff[4, 6] = 14.5;
+
+            coeff[5, 0] = 2.0;
+            coeff[5, 1] = 3.2;
+            coeff[5, 2] = 3.35;
+            coeff[5, 3] = 1.9;
+            coeff[5, 4] = 1.7;
+            coeff[5, 5] = 2.5;
+            coeff[5, 6] = 16.0;
+
+            coeff[6, 0] = 2.15;
+            coeff[6, 1] = 3.15;
+            coeff[6, 2] = 3.2;
+            coeff[6, 3] = 1.75;
+            coeff[6, 4] = 1.8;
+            coeff[6, 5] = 2.5;
+            coeff[6, 6] = 16.5;
+
+            coeff[7, 0] = 2.3;
+            coeff[7, 1] = 3.0;
+            coeff[7, 2] = 3.1;
+            coeff[7, 3] = 1.7;
+            coeff[7, 4] = 1.75;
+            coeff[7, 5] = 2.6;
+            coeff[7, 6] = 18;
+
+            coeff[8, 0] = 2.45;
+            coeff[8, 1] = 2.95;
+            coeff[8, 2] = 2.65;
+            coeff[8, 3] = 1.75;
+            coeff[8, 4] = 2.05;
+            coeff[8, 5] = 3.6;
+            coeff[8, 6] = 18.5;
+
+            coeff[9, 0] = 2.6;
+            coeff[9, 1] = 2.9;
+            coeff[9, 2] = 2.7;
+            coeff[9, 3] = 1.8;
+            coeff[9, 4] = 2.0;
+            coeff[9, 5] = 3.1;
+            coeff[9, 6] = 17.0;
+
+            coeff[10, 0] = 2.95;
+            coeff[10, 1] = 3.0;
+            coeff[10, 2] = 3.0;
+            coeff[10, 3] = 1.6;
+            coeff[10, 4] = 1.8;
+            coeff[10, 5] = 2.95;
+            coeff[10, 6] = 21;
+
+            coeff[11, 0] = 3.5;
+            coeff[11, 1] = 3.2;
+            coeff[11, 2] = 2.35;
+            coeff[11, 3] = 1.7;
+            coeff[11, 4] = 1.9;
+            coeff[11, 5] = 2.85;
+            coeff[11, 6] = 19.5;
+
+            coeff[12, 0] = 5.1;
+            coeff[12, 1] = 3.8;
+            coeff[12, 2] = 1.95;
+            coeff[12, 3] = 1.9;
+            coeff[12, 4] = 1.8;
+            coeff[12, 5] = 2.65;
+            coeff[12, 6] = 15;
+
+            coeff[13, 0] = 5.5;
+            coeff[13, 1] = 3.85;
+            coeff[13, 2] = 1.75;
+            coeff[13, 3] = 1.85;
+            coeff[13, 4] = 1.7;
+            coeff[13, 5] = 2.8;
+            coeff[13, 6] = 14.5;
+
+            coeff[14, 0] = 7.5;
+            coeff[15, 1] = 4.25;
+            coeff[15, 2] = 1.45;
+            coeff[15, 3] = 1.95;
+            coeff[15, 4] = 1.65;
+            coeff[15, 5] = 2.45;
+            coeff[15, 6] = 12.5;
+
+            coeff[15, 0] = 11.25;
+            coeff[15, 1] = 5.85;
+            coeff[15, 2] = 1.3;
+            coeff[15, 3] = 2.15;
+            coeff[15, 4] = 1.55;
+            coeff[15, 5] = 2.15;
+            coeff[15, 6] = 10.05;
+
+ 
+        }
+
+        public static double getCoefficient(Match m, int type)//dava vrednost na koef. za daden natprevar
+        {
+            random = new Random();
+            type--;
+            int ratingDiff = (int)(m.HomeTeam.Rating - m.GuestTeam.Rating);
+            for (int i = 0; i < lowerBound.Length; i++)
+            {
+                if (ratingDiff > lowerBound[i])
+                {
+                    return coeff[i, type] + diff[random.Next(3)];
+                }
+            }
+            return -1;
+        }
+
+        public double[] getCoefficientsForMatch(Match m)//dobivanje na site koefficienti za daden natprevar
+        {              //koef. so id = i od bazata ovde ima id = i - 1 
+            random = new Random();
+            double[] ret = new double[7];
+            int ratingDiff = (int)(m.HomeTeam.Rating - m.GuestTeam.Rating);
+            for (int i = 0; i < lowerBound.Length; i++)
+            {
+                if (ratingDiff > lowerBound[i])
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        ret[j] = coeff[i, j] + random.Next(3);
+                    }
+                    return ret;
+                }
+                
+                    
+            }
+            return null;
+        }
+
+    }
+}
